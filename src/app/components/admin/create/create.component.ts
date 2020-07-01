2532a70a922238a331985a76254e47d0c63d4e14
Thomas Rekittke <betterobjects@mac.com>
2020-07-01 13:43:35 +0200

    ok

--- src/app/components/admin/create/create.component.ts
+++ src/app/components/admin/create/create.component.ts
@@ -28,19 +28,15 @@ export class CreateComponent extends AdminEntityComponent {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
+    if( this.validateForm.valid ) this.createMutation();
   }
 
   errorTip(field:FieldConfigType):string {
     return 'Invalid';
   }
 
-  onSave(){
-    this.createMutation();
-  }
-
-  onCancel(){
-    this.router.navigate(['/admin', this.path ] );
-  }
+  onSave = () => this.submitForm();
+  onCancel = () => this.router.navigate(['/admin', this.path ]);
 
   protected setDefaults( config:EntityConfigType ):EntityConfigType {
     if( ! _.has(config, 'create') ) _.set( config, 'create', {} );
@@ -60,18 +56,21 @@ export class CreateComponent extends AdminEntityComponent {
             attribute
             violation
           }
+          ${this.config.typeQuery}{ id }
         }
       }`;
     this.apollo.mutate({
       mutation: createMutation,
-      variables: { input: this.getItemInput( this.item ) },
+      variables: { input: this.getItemInput() },
       errorPolicy: 'all'
     }).subscribe(({data, errors}) => {
-      const violations = _.get( data, 'validationViolations' );
+      const result = _.get( data, this.config.createMutation );
+      const violations = _.get( result, 'validationViolations' );
       if( _.size(errors) > 0 ) console.error( {errors})
       if( _.size( violations ) === 0 ) {
-        this.message.info(`This ${this.title('edit')} was updated!` );
-        setTimeout( ()=> this.gotoShow(), 500 );
+        this.message.info(`This ${this.title('edit')} was created!` );
+        const id = _.get( result, [this.config.typeQuery, 'id'] );
+        setTimeout( ()=> this.gotoShow( this.path, id ), 500 );
       } else {
         this.message.error( _.join(violations, '\n') );
       }
@@ -81,11 +80,6 @@ export class CreateComponent extends AdminEntityComponent {
     });
   }
 
-  protected getItemInput( item:any ){
-    return _.pick( item, _.keys(this.config.fields) );
-  }
-
-
   protected buildForm(){
     const definition = _.reduce( this.config.create.fields, (definition, field:FieldConfigType) => {
       const validators = this.required( field ) ? [Validators.required] : [];
@@ -93,4 +87,6 @@ export class CreateComponent extends AdminEntityComponent {
     }, {});
     this.validateForm = this.fb.group(definition);
   }
+
+  protected getItemInput = () => this.validateForm.value;
 }
