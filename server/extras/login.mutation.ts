60ddf7069334bb43a66a2b5cefe3cf0ea171baa1
Thomas Rekittke <betterobjects@mac.com>
2020-08-14 15:20:26 +0200

    go

--- server/extras/login.mutation.ts
+++ server/extras/login.mutation.ts
@@ -19,35 +19,19 @@ export class LoginMutation {
           seeds: {
             admin: {
               login: 'admin',
-              password: () => {
-                const salt = bcrypt.genSaltSync(10);
-                const hash = bcrypt.hashSync('admin', salt);
-                return hash;
-              }
+              password: this.password('admin')
             },
             user1: {
               login: 'user1',
-              password: () => {
-                const salt = bcrypt.genSaltSync(10);
-                const hash = bcrypt.hashSync('user1', salt);
-                return hash;
-              }
+              password: this.password('user1')
             },
             user2: {
               login: 'user2',
-              password: () => {
-                const salt = bcrypt.genSaltSync(10);
-                const hash = bcrypt.hashSync('user2', salt);
-                return hash;
-              }
+              password: this.password('user2')
             },
             user3: {
               login: 'user3',
-              password: () => {
-                const salt = bcrypt.genSaltSync(10);
-                const hash = bcrypt.hashSync('user3', salt);
-                return hash;
-              }
+              password: this.password('user3')
             }
           },
           extendFn: ( context:Context ) => {
@@ -67,11 +51,17 @@ export class LoginMutation {
     }
   }
 
-  static async login( context:Context, login:string, password:string ):Promise<boolean> {
+  private static async login( context:Context, login:string, password:string ):Promise<boolean> {
     const entity = context.entities['User'];
     if( ! entity ) return context.warn( `no 'User' type found`, false );
     const user = await entity.findOneByAttribute( { login } );
     if( ! user ) return false;
     return bcrypt.compare( password, user.item.password );
+  }
+
+  private static password( password:string ):string {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync( password, salt);
+    return hash;
   }
 }
