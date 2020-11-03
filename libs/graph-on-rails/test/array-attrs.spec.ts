7bf6028d3d4cb6ecf638fabc960038b9640cec56
Thomas Rekittke <betterobjects@mac.com>
2020-11-03 09:54:33 +0100

    ok

--- libs/graph-on-rails/test/array-attrs.spec.ts
+++ libs/graph-on-rails/test/array-attrs.spec.ts
@@ -1,5 +1,6 @@
-import { DomainConfiguration } from 'core/domain-configuration';
 import { printSchema } from 'graphql';
+
+import { DomainConfiguration } from '../core/domain-configuration';
 import { Runtime } from '../core/runtime';
 import { Seeder } from '../core/seeder';
 
@@ -8,45 +9,57 @@ const domainConfiguration:DomainConfiguration = {
     User: {
       attributes: {
         username: 'string!',
-        roles: '[string]'
+        roles: '[string]',
+        someIds: {
+          type: 'int',
+          list: true
+        }
       },
       seeds: {
         admin: {
           username: 'admin',
-          roles: ['user', 'admin']
+          roles: ['user', 'admin'],
+          someIds: [1, 2, 3]
         },
         regular: {
           username: 'regular',
-          roles: ['user']
+          roles: ['user'],
+          someIds: [23, 42]
         }
       }
     }
   }
 };
 
-
 describe('Permissions', () => {
 
   let runtime!:Runtime;
-
 
   const consoleSpy = jest.spyOn(console, 'warn').mockImplementation();
 
   beforeAll( async () => {
     runtime = await Runtime.create({ name: 'test:permissions', domainDefinition: domainConfiguration });
     await Seeder.create( runtime ).seed( true );
-    console.log( printSchema( runtime.schema ) );
+    if( false ) console.log( printSchema( runtime.schema ) );
   })
 
   //
   //
-  it('should', async ()=> {
+  it('should have array values from seed', async ()=> {
     const user = runtime.entities['User'];
     const admin = await user.findOneByAttribute( {username: 'admin'} );
-    console.log( admin?.item.roles );
-    expect( admin?.item.roles ).toHaveLength( 2 );
     expect( admin?.item.roles ).toEqual(['user', 'admin']);
+    expect( admin?.item.someIds ).toEqual([1,2,3]);
   });
 
+  //
+  //
+  it('should store and read array values', async ()=> {
+    const user = runtime.entities['User'];
+    await user.accessor.save( { username: 'another', roles: ['foo', 'bar'], someIds: [23]})
+    const another = await user.findOneByAttribute( {username: 'another'} );
+    expect( another?.item.roles ).toEqual(['foo', 'bar']);
+    expect( another?.item.someIds ).toEqual([23]);
+  });
 
 })
